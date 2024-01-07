b239b1f26fd9189d40cddd98d1f3dba936dd15ae
Arda Eren <70160402+DeCEll-1@users.noreply.github.com>
2024-01-07 21:44:22 +0300

    forgoring try catch was a mistake

--- idkWhatImDoing.py
+++ idkWhatImDoing.py
@@ -6,18 +6,13 @@ import json
 redPixelThresh = [120, 20, 20]
 greenPixelThresh = [20, 120, 20]
 bluePixelThresh = [20, 20, 120]
-
 orangePixelThresh = [120, 120, 20]
 yellowPixelThresh = [200, 120, 20]
 purpPixelThresh = [120, 20, 120]
-
 whitePixelThresh = [200, 200, 200]
-
 spiceImage = cv.imread("StarscapeMapSpice.png", cv.IMREAD_COLOR)
 spiceImage = cv.cvtColor(spiceImage, cv.COLOR_BGR2RGB)
-
 (height, width, depth) = spiceImage.shape
-
 pixelCoordinateList = [["", -1, -1]]  # color as string, pixelx, pixelY
 # yeah
 # whats that
@@ -25,102 +20,103 @@ pixelCoordinateList = [["", -1, -1]]  # color as string, pixelx, pixelY
 # yeah?
 # json objects?
 
+try:
+    for pixelX in range(0, width):  #  no :sleeping_accomodation:
+        for pixelY in range(0, height):
+            (pixelR, pixelG, pixelB) = spiceImage[pixelX, pixelY]
 
-for pixelX in range(0, width):  #  no :sleeping_accomodation:
-    for pixelY in range(0, height):
-        (pixelR, pixelG, pixelB) = spiceImage[pixelX, pixelY]
+            if (
+                helper.helper.CheckPixelThreshold(
+                    thresholdValues=redPixelThresh,
+                    pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
+                    pixelCoordinateList=pixelCoordinateList,
+                )
+                == "Red"
+            ):  # red
+                print("Found Red Pixel At: " + str([pixelX, pixelY]))
+            #
 
-        if (
-            helper.helper.CheckPixelThreshold(
-                thresholdValues=redPixelThresh,
-                pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
-                pixelCoordinateList=pixelCoordinateList,
-            )
-            == "Red"
-        ):  # red
-            print("Found Red Pixel At: " + str([pixelX, pixelY]))
+            if (
+                helper.helper.CheckPixelThreshold(
+                    thresholdValues=greenPixelThresh,
+                    pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
+                    pixelCoordinateList=pixelCoordinateList,
+                )
+                == "Green"
+            ):  # red
+                print("Found Green Pixel At: " + str([pixelX, pixelY]))
+            #
+
+            if (
+                helper.helper.CheckPixelThreshold(
+                    thresholdValues=bluePixelThresh,
+                    pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
+                    pixelCoordinateList=pixelCoordinateList,
+                )
+                == "Blue"
+            ):  # red
+                print("Found Blue Pixel At: " + str([pixelX, pixelY]))
+            #
+
+            # combined colors
+
+            if (
+                helper.helper.CheckPixelThreshold(
+                    thresholdValues=orangePixelThresh,
+                    pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
+                    pixelCoordinateList=pixelCoordinateList,
+                )
+                == "Orange"
+            ):  # red
+                print("Found Orange Pixel At: " + str([pixelX, pixelY]))
+            #
+
+            if (
+                helper.helper.CheckPixelThreshold(
+                    thresholdValues=yellowPixelThresh,
+                    pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
+                    pixelCoordinateList=pixelCoordinateList,
+                )
+                == "Yellow"
+            ):  # red
+                print("Found Yellow Pixel At: " + str([pixelX, pixelY]))
+            #
+
+            if (
+                helper.helper.CheckPixelThreshold(
+                    thresholdValues=purpPixelThresh,
+                    pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
+                    pixelCoordinateList=pixelCoordinateList,
+                )
+                == "Purple"
+            ):  # red
+                print("Found Purple Pixel At: " + str([pixelX, pixelY]))
+            #
+
+            # white
+
+            if (
+                helper.helper.CheckPixelThreshold(
+                    thresholdValues=whitePixelThresh,
+                    pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
+                    pixelCoordinateList=pixelCoordinateList,
+                )
+                == "White"
+            ):  # red
+                print("Found White Pixel At: " + str([pixelX, pixelY]))
+            #
         #
 
-        if (
-            helper.helper.CheckPixelThreshold(
-                thresholdValues=greenPixelThresh,
-                pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
-                pixelCoordinateList=pixelCoordinateList,
-            )
-            == "Green"
-        ):  # red
-            print("Found Green Pixel At: " + str([pixelX, pixelY]))
-        #
 
-        if (
-            helper.helper.CheckPixelThreshold(
-                thresholdValues=bluePixelThresh,
-                pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
-                pixelCoordinateList=pixelCoordinateList,
-            )
-            == "Blue"
-        ):  # red
-            print("Found Blue Pixel At: " + str([pixelX, pixelY]))
-        #
+finally:
+    testDict = {"SystemList": pixelCoordinateList}
 
-        # combined colors
-
-        if (
-            helper.helper.CheckPixelThreshold(
-                thresholdValues=orangePixelThresh,
-                pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
-                pixelCoordinateList=pixelCoordinateList,
-            )
-            == "Orange"
-        ):  # red
-            print("Found Orange Pixel At: " + str([pixelX, pixelY]))
-        #
-
-        if (
-            helper.helper.CheckPixelThreshold(
-                thresholdValues=yellowPixelThresh,
-                pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
-                pixelCoordinateList=pixelCoordinateList,
-            )
-            == "Yellow"
-        ):  # red
-            print("Found Yellow Pixel At: " + str([pixelX, pixelY]))
-        #
-
-        if (
-            helper.helper.CheckPixelThreshold(
-                thresholdValues=purpPixelThresh,
-                pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
-                pixelCoordinateList=pixelCoordinateList,
-            )
-            == "Purple"
-        ):  # red
-            print("Found Purple Pixel At: " + str([pixelX, pixelY]))
-        #
-
-        # white
-
-        if (
-            helper.helper.CheckPixelThreshold(
-                thresholdValues=whitePixelThresh,
-                pixel=(pixelR, pixelG, pixelB, pixelX, pixelY),
-                pixelCoordinateList=pixelCoordinateList,
-            )
-            == "White"
-        ):  # red
-            print("Found White Pixel At: " + str([pixelX, pixelY]))
-        #
+    # what that does?
+    with open("SystemDataOutput.json") as file:
+        json.dump(testDict, file, indent=4)
+    # ima play ss13 frfr
+    # space station 13
+    # watch sseths video on it
+    # crazy? i was crazy once
     #
-
-
-testDict = {"SystemList": pixelCoordinateList}
-
-# what that does?
-with open("SystemDataOutput.json") as file:
-    json.dump(testDict, file, indent=4)
-# ima play ss13 frfr
-# space station 13
-# watch sseths video on it
-# 
-# 
-# 
+    #
